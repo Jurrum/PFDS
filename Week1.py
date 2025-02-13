2cd6281db7640f6bc52c5ff53fca4fea60562cdb
Jurrum <jbdboumans@gmail.com>
2025-02-13 09:14:07 +0100

    Init commit

---
+++ Week1.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+"""
+Week 1
+
+Research setup
+- Formulate, document, evaluate, repeat
+- CBOOSAR - Hypothesis measure (septagon)
+- 
+
+- 
+
+ git config --global user.email "jbdboumans@gmail.com"
+  git config --global user.name "Your Name"
+
+"""
