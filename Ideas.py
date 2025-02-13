3bcb5f3a7403f196f7c1f936664d670b2d2b5fc2
Jurrum <jbdboumans@gmail.com>
2025-02-13 13:08:39 +0100

    Ideas added

---
+++ Ideas.py
@@ -0,0 +1,7 @@
+"""
+Ideas for Social media feed futures
+
+- Let an Agent use Social media, and see what it learns
+- Create a tamagochi comapanion that dies with too much SM use
+- Let two agents try to persuade each other of oppposing views
+"""
