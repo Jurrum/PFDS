46530ea39a6aec3e1aed2bdd3d81ccdb464f1152
Jurrum <jbdboumans@gmail.com>
2025-02-13 09:29:42 +0100

    Gotta stay committed

---
+++ Notes/Week1.py
@@ -0,0 +1,41 @@
+
+"""
+Week 1
+
+Research setup
+- Formulate, document, evaluate, repeat
+- CBOOSAR - Hypothesis measure (septagon)
+- 
+
+Emergence
+- as a feature of practice ased design research (Gaver et all 2023)
+- Design research droft from one idea to another and from method to method. 
+- 
+
+Night vs Day science
+- Night science is about the messy stuff, rrying and failing
+- 
+
+RTD - Research through design
+knowledge the you create during this course merges throufh engaging in design activities
+Prototypes are more than products, they are research artefacts
+
+"Permission ot muck about" A movie about the messy process of RTF
+
+Design research is about the process, not the product
+involves a lot of trial and error
+ie. the process of making a prototype is more important than the prototype itself
+
+-- Panel Discussion --
+
+
+
+"""
+
+def hello():
+    print("Hello World")
+
+if __name__ == "__main__":
+  hello()
+  for i in range(10):
+    print(i)
