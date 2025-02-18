6adf9be92349ac4a1278af51af8ce1b7ceaea1c9
Jurrum <jbdboumans@gmail.com>
2025-02-18 14:48:02 +0100

    Week 2: Lecture notes

---
+++ Notes/Week 2 - Ideate and tools.py
@@ -0,0 +1,88 @@
+""" 
+Week 2 notes
+
+Lecture 
+- Finding and Refining gaps
+- First deliverable: abstract
+    - Motvations: What are mine? Why am I intrerested in this topic?
+    - What are the gaps in the literature? What are the questions that have not been answered?
+    - look at recmmondations from other papers with 'future research' section
+    - Can you bring a unique perspective to the topic?
+    - Write down ideas, scriblles, thoughts, etc.
+    - 
+
+"Machine learning in living organisms.." -so normal learning?
+
+
+Why are they just showcasing research that is only related to veyr specific topics?? 
+As if we're not able to think og these motivations ourselves. 
+It's just showing their own research. It doesn't deviate from the normal prcatice of research...
+
+For sustainable 3D printing: You're just going to be doing the things that the researcher does not
+want to do themselves. all these tests are tedious work. Gathering these material insights is 
+in any normal siuation done by a large company that specilizes in this. Not by a swarm of students.
+
+
+- Where do ideas come from?
+    - Inquiry vs invention
+    - From idea to solution
+    - from problem to solution
+- Vuzix Z100: example of smart glasses
+- Prbolem solving
+    - People dont want a quarter inch drill, they want a quarter inch hole
+    - Emphatize, deinge, ideate, prototype, test
+    - how to not reinvent the wheel
+- How do you know what the gaps are in the knowledge?
+    - What are the boundaries of your searc,?
+    What is in/out of your preokct?
+    What are keywords people are using?
+    What are you looking for? Detials of atechnique or design method?
+- Keyword research
+    - User BOOL operators, AND, OR, NOT
+    - use this in your query
+    - Acually use AND, OR, NOT in your search in Scholar
+    - use ~ for synonyms!!
+    - Also jus search for operators
+    - 
+- Where to search
+    - HCI: ACM library
+    - IEEE
+    = Google Scholar
+    - Scopus
+    - Web of Science
+    - Springer
+    - Science Direct
+    - JSTOR
+    - ResearchGate
+
+- Tools to help
+    - Mendeley
+    - !!!Connected papers!!! - visualized a networked map
+    - Mendely: can see what other people are reading
+    - Zotero
+    - Endnote
+    - RefWorks
+    - Plugins with Google docs to automate workflow
+
+-Turning it into words
+    - Move step model
+        Move 1 establishing the field
+            Scope of the research and what has been done
+            Step 1: Claiming centrality
+                Interesintg, sginingficant, important, relevant, timely, etc.
+            Step 2 stating the current knowledge and degining terms
+                Generally, such as, common, for example, most people,
+            Step 3: Reviewing previous research
+                Much research, has inbeestigated, found, arues, has been concluded, 
+                has been studied, stated, claims, studied
+        Move 2: establiching the nieche
+            Step 1: counter claiming
+            Step 2: indicating a gap
+            Step 3: Raising questions
+            Step 4, cotinuing a tradition
+        Move 3: Occupying the niche
+    - Do this for each category/cluster of your research
+    - Summarise into one coherent "Gap"
+    - 
+    
+"""
