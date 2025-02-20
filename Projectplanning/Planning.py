984a5ddc93c6d7d6e6e9471aa7a2b49421a7d296
Jurrum <jbdboumans@gmail.com>
2025-02-20 12:19:53 +0100

    Homework, Deadline week 3

---
+++ Projectplanning/Planning.py
@@ -0,0 +1,10 @@
+"""
+Homework for week 3
+- Everyone finds 5 relevant articles for the project
+- Add references in the 'Literature.text' file so eveyone papers
+- Add keywords to 'keywords.txt' file
+    - APA + Links for each paper!!!
+    + 2 sentences that summarize the content, why is it relevant
+
+
+"""
