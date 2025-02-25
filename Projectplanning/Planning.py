cbd9a5d5d8cd383ecf103af8c48e63dd621ba71e
Jurrum <jbdboumans@gmail.com>
2025-02-25 17:07:47 +0100

    Added literature

--- Projectplanning/Planning.py
+++ Projectplanning/Planning.py
@@ -1,4 +1,10 @@
 """
+Homework for week 4
+See abstract in: https://docs.google.com/document/d/1-MgIn8xcxr1d8Ws550-PS3s8sHvmEv08Nl_lUqoQunE/edit?tab=t.0#heading=h.qy9cxsazakfk
+- Write abstract
+- Add references to the 
+- 
+
 Homework for week 3
 - Everyone finds 5 relevant articles for the project
 - Add references in the 'Literature.text' file so eveyone papers
